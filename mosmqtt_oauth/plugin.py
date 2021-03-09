23e24cf3fb2a74ce4715b29224d968ea190b1bc4
Raja Ravi <rravi@logitech.com>
2021-03-09 15:29:18 +0530

    initial working commit

---
+++ mosmqtt_oauth/plugin.py
@@ -0,0 +1,100 @@
+import os
+import logging
+import requests
+
+import redis
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+REDIS_CONN = None
+
+
+def user_cache_key(token): return 'mosq:' + token
+def user_acl_cache_key(token): return user_cache_key(token) + ':acl'
+
+
+def userDetails(token: str) -> dict:
+    AUTH_URL = os.environ['AUTH_URL']
+
+    try:
+        response = requests.get(
+            AUTH_URL,
+            headers={
+                'Authorization': 'Bearer {}'.format(token),
+                'Content-Type': 'application/json'
+            })
+        response.raise_for_status()
+    except requests.HTTPError as e:
+        logger.exception(e)
+        return {}
+    data = response.json()
+    if not 'topics' in data or not 'email' in data:
+        logger.error("Invalid response from from auth system: %s", data)
+        return {}
+    return data
+
+
+def plugin_init(opts):
+    REDIS_HOST = os.environ['REDIS_HOST']
+    REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
+    REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
+    REDIS_SSL = os.environ.get('REDIS_SSL') in {
+        '1', 'true', 'True', 'TRUE', None}
+    REDIS_DB = os.environ.get('REDIS_DB', 0)
+
+    global REDIS_CONN
+
+    REDIS_CONN = redis.StrictRedis(
+        host=REDIS_HOST,
+        ssl=REDIS_SSL,
+        port=REDIS_PORT,
+        db=REDIS_DB,
+        password=REDIS_PASSWORD,
+    )
+    logger.info('redis initialized: %s %s', REDIS_HOST, REDIS_PORT)
+
+
+def unpwd_check(username: str, password: str) -> bool:
+    token = username
+    user = userDetails(token)
+    if not user:
+        logger.error("Login failed for User: %s", password)
+        return False
+
+    REDIS_CONN.hset(user_cache_key(token), 'user', user["email"])
+    REDIS_CONN.delete(user_acl_cache_key(token))
+    for allowedTopic in user["topics"]:
+        REDIS_CONN.lpush(user_acl_cache_key(token), allowedTopic)
+
+    return True
+
+
+def acl_check(client_id: str, username: str, topic: str, access: str, payload) -> bool:
+    import mosquitto_auth
+    if username is None:
+        logger.error('Authentication required for acl check')
+        return False
+    logger.debug("Checking User ACL")
+
+    user = REDIS_CONN.hget(user_cache_key(username), 'user')
+    if user:
+        allowedTopics = REDIS_CONN.lrange(user_acl_cache_key(username), 0, -1)
+        if not allowedTopics:
+            logger.warn('No ACL found for User: %s', user)
+            return False
+
+        matches = False
+        for allowedTopic in allowedTopics:
+            matches = mosquitto_auth.topic_matches_sub(
+                allowedTopic.decode(), topic)
+            if matches:
+                break
+        if matches is False:
+            logger.info(
+                "User: %s requested topic: %s not allowed. Allowed Topics: %s", user, topic, allowedTopics)
+
+    logger.info('ACL: user=%s topic=%s, matches = %s, payload = %r' %
+                (user, topic, matches, payload))
+    return matches
---
+++ setup.py
@@ -0,0 +1,47 @@
+
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+from mosmqtt_oauth import __version__, __maintainer__, __email__, __license__
+
+long_description = '''
+Documentation
+-------------
+Full documentation is available on `Github`_.
+.. _`Github`: https://github.com/rajaravi1/mosmqtt-oauth
+'''
+
+install_requires = ['redis==3.5.3', 'requests==2.22.0']
+
+
+setup(
+    name='mosmqtt-oauth',
+    version=__version__,
+    author=__maintainer__,
+    author_email=__email__,
+    url='https://github.com/rajaravi1/mosmqtt-oauth',
+    description='OAuth plugin module for MOSQUITTO MQTT',
+    long_description=long_description,
+    license=__license__,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=install_requires,
+    # extras_require={'test': tests_require},
+    # cmdclass={'test': PyTest},
+    # tests_require=tests_require,
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        'Development Status :: 5 - Production/Stable',
+        "License :: OSI Approved :: MIT License",
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
